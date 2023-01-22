54f64ad1bea1579a427ec73dc396c22169c3b687
Nikolay Yatsenko <yatsenkokolyan@gmail.com>
2023-01-22 18:16:07 +0600

    Header, Intro, About - adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,25 @@ for (let anchor of anchors) {
             behavior: 'smooth',
             block: 'start'
         });
+        document.querySelector('.header__nav').classList.remove('header__nav-active');
+        document.querySelector('body').classList.remove('body_noscroll');
     });
 };
+
+// burger handler
+
+(function () {
+    const menu = document.querySelector('.header__nav');
+    const burgerItem = document.querySelector('.burger');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const noScroll = document.querySelector('body');
+    burgerItem.addEventListener('click', () =>{
+        menu.classList.add('header__nav-active');
+        noScroll.classList.add('body_noscroll');
+    });
+    menuCloseItem.addEventListener('click', () =>{
+        menu.classList.remove('header__nav-active');
+        noScroll.classList.remove('body_noscroll');
+    });
+
+}());
